761eb560e5bb402c9313489090ac32e851385b3f
gags22 <73459594+gags22@users.noreply.github.com>
2021-02-03 10:46:54 +0530

    Update add_with_1_function.c

--- add_with_1_function.c
+++ add_with_1_function.c
@@ -2,12 +2,12 @@
 #include<stdio.h>
 int main()
 {
- int number1,number2,c = 0;
+ float number1,number2,c = 0;
 printf("Enter number1:");
-scanf("%d",&number1);
+scanf("%f",&number1);
 printf("Enter number2:");
-scanf("%d",&number2);
+scanf("%f",&number2);
 c = number1+number2;
-printf("sum of %d and %d is %d",number1,number2,c);
+printf("sum of %f and %f is %f",number1,number2,c);
 return 0;
 }
