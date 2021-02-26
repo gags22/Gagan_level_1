4db1867b8e5a968f74a90677ac28f091336347e5
gags22 <73459594+gags22@users.noreply.github.com>
2021-02-26 06:18:48 +0530

    Update distance_with_structures.c

--- distance_with_structures.c
+++ distance_with_structures.c
@@ -4,27 +4,40 @@
 struct distance
 {
     float x1;
-    float x2;
     float y1;
+    float x2;
     float y2;
     float d1;
+    float n;
+   
 };
-void display_distance(struct distance d);
-float main()
+void display(struct distance d);
+float get_input(char a)
 {
     struct distance d;
-    printf("Enter x coordinates:\n");
-    scanf("%f%f",&d.x1,&d.x2);
-    printf("Enter y coordinates:\n");
-    scanf("%f%f",&d.y1,&d.y2);
-   
-    display_distance(d);
-    return 0;
-    
+    printf("Enter value of %c:\n",a);
+    scanf("%f",&d.n);
+    return d.n;
 }
-void display_distance(struct distance d)
+float find_dist(float x1,float y1,float x2,float y2)
 {
     d.d1 = sqrt((d.x2-d.x1)*(d.x2-d.x1)+(d.y2-d.y1)*(d.y2-d.y1));
-    printf("Distance bewteen two numbers is %f",d.d1);
-    
+    return d.d1;
 }
+void display_distance( )
+{
+  printf("Distance between two points is %f",d.d1);
+}
+float main()
+{
+    float x,y,z,v,w;
+    struct distance d;
+    x = get_input('x');
+    y = get_input('y');
+    z = get_input('x');
+    v = get_input('y');
+    w = find_dist(x,y,z,v);
+    display_distance(w);
+    return 0;
+}
+
